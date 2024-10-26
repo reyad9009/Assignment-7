c7a9b9e7fef2ed2d759458e57a4750b7f16d768f
reyad9009 <tarekgithub09@gmail.com>
2024-10-26 23:26:29 +0600

    upadate 2

--- src/components/Selected/Selected.jsx
+++ src/components/Selected/Selected.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const Selected = ({handleSelected, choosePlayer, deleteSelectedPlayer}) => {
     return (
-        <div className="  flex items-center justify-center h-lvh">
+        <div className="  flex items-center justify-center">
             <div className="flex flex-col w-12/12 w-[70%]">
                 <h1>Selected Player({choosePlayer.length}/6)</h1>
                 <div className="">
@@ -12,7 +12,7 @@ const Selected = ({handleSelected, choosePlayer, deleteSelectedPlayer}) => {
                             choosePlayer.map((choosePlayers, playerId)=> <Select key={playerId} select={choosePlayers} deleteSelectedPlayer={deleteSelectedPlayer}></Select>)
                         }
                     </div>
-                    <button onClick={handleSelected} className="btn">Add More Player</button>
+                    <button onClick={handleSelected} className="btn mb-96 bg-[#d9ef28] outline-offset-4 outline outline-[#d9ef28]">Add More Player</button>
                 </div>
             </div>
               
